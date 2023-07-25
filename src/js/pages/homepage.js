bc9b5a12e69b82700c2fc426d7f3dcde9c3c3ff2
DanyWB <dany6191@mail.ru>
2023-07-25 18:45:06 +0300

    add content page and fix

--- src/js/pages/homepage.js
+++ src/js/pages/homepage.js
@@ -7,3 +7,57 @@ let header = document.querySelectorAll('.form-select__header');
  }
 }
 //-----------------------------//
+// let x = 0;
+// const slider = document.querySelector('.slider__list')
+// slider.addEventListener('wheel', function(event) {
+
+  
+//   x += event.deltaY;
+//   slider.style.transform = 'translateX(' + x +'px'+ ')';
+//   console.log(x);
+
+// })
+
+
+  
+  var lastX = 0; //последняя координата x мыши
+  var SliderScroll= false; //требуется ли анимация? 
+
+  $(document).mousemove(function(e) {
+      lastX = e.clientX; //обновили горизонтальное положение курсора
+  });
+  
+
+ // как только курсор оказывается в элементе news-slider начинаем анимацию прокрутки
+  
+  $(".slider__list").mouseenter(function(e) {
+  
+      SliderScroll= true;
+  
+      NewsSliderScroll();//запуск самой анимации
+  
+  });
+  
+
+  
+  $(".slider__list").mouseleave(function(e) {
+  
+      SliderScroll= false;//сбрасываем для выхода из рекурсии
+  
+  });
+  
+  
+  function NewsSliderScroll() {
+  
+    setTimeout(function() {
+                var speed = (0.5 - ($(window).width() - lastX) / $(window).width()).toFixed(2);
+                var step = $(".slider__list").scrollLeft() + 50 * speed;
+                $(".slider__list").scrollLeft(step);
+                if (SliderScroll) NewsSliderScroll();
+            }, 20);
+    
+  
+  }
+  
+  
+  
