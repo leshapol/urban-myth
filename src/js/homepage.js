53d5fe32c25df801ebbe3499298953e1cb385e47
leshapol <leshapol@gmail.com>
2023-07-30 00:13:10 +0300

    homepage update

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -1,4 +1,3 @@
-@@include('./libs/lazysizes.min.js', {})
-@@include('pages/header.js', {})
-
+@@include('libs/lazysizes.min.js', {})
+@@include('common/swipe-slider.js', {})
 @@include('pages/homepage.js', {})
