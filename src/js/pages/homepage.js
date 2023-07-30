e962a4139f24091633d0a0947e8f05534c729eb5
DanyWB <dany6191@mail.ru>
2023-07-30 23:55:51 +0300

    add beta-slider for about-block

--- src/js/pages/homepage.js
+++ src/js/pages/homepage.js
@@ -1,13 +1,19 @@
 let pag = document.getElementsByClassName("pag__item");
 let image = document.querySelector(".about__image > img");
+let imgarr = new Array("img/about.jpg", "img/aboutus.jpg", "img/aboutus2.jpg");
 
-let imgarr = new Array("card.jpg", "card2.jpg", "card3.jpg"); //        Добавте свои картинки через запятую
 function imgsrc(i) {
-  image.src = imgarr[i + 1];
-  console.log(imgarr[i + 1]);
+  image.src = imgarr[i - 1];
 }
-
 $(".pag__item").on("click", function () {
+  const delay = 5000;
   let i = $(this).data("num");
+  let pags = document.querySelectorAll(".pag__item.pag__item_active");
+
+  for (let i = 0; i < pags.length; i++) {
+    pags[i].classList.remove("pag__item_active");
+  }
+  this.classList.add("pag__item_active");
+
   imgsrc(i);
 });
