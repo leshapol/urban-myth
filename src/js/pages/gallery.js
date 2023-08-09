a5b3a03594fd938955298fbf7a63eb5f93a6635c
DanyWB <dany6191@mail.ru>
2023-08-09 13:48:01 +0300

    add popup for gallery-filters

--- src/js/pages/gallery.js
+++ src/js/pages/gallery.js
@@ -1,19 +1,19 @@
 //Gallery form select script//
-let header = document.querySelectorAll(".filters__header");
-for (let heade of header) {
-  heade.onclick = function () {
-    heade.classList.toggle("filters__header_active");
-  };
-}
+// let header = document.querySelectorAll(".filters__header");
+// for (let heade of header) {
+//   heade.onclick = function () {
+//     heade.classList.toggle("filters__header_active");
+//   };
+// }
 //-----------------------------//
-let open = document.querySelector(".catalog__modal-open");
-let close = document.querySelector(".filters__close");
-let filters = document.querySelector(".filters");
+// let open = document.querySelector(".catalog__modal-open");
+// let close = document.querySelector(".filters__close");
+// let filters = document.querySelector(".filters");
 
-open.onclick = function () {
-  filters.classList.add("filters_active");
-};
+// open.onclick = function () {
+//   filters.classList.add("filters_active");
+// };
 
-close.onclick = function () {
-  filters.classList.remove("filters_active");
-};
+// close.onclick = function () {
+//   filters.classList.remove("filters_active");
+// };
